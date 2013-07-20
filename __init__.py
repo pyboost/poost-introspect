9547c4cb6975e6b4858b7168d5a28ab2d9c373fe
Pengkui Luo <pengkui.luo@gmail.com>
2013-07-19 21:29:19 -0700

    --- Switched to year-based versioning system, e.g. 0.13.1-a1.

--- __init__.py
+++ __init__.py
@@ -1,7 +1,22 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2013 Pengkui Luo <pengkui.luo@gmail.com>
+# Created 06/23/2013, updated 07/19/2013
+#
 """
 Python Introspection package.
 
 """
+from __future__ import absolute_import
 
-__version__ = '0.1.0-a1'
+print('Executing %s' %  __file__)
 
+import sys
+if not (2, 6) <= sys.version_info < (3, ):
+    raise ImportError("CPython 2.6.x or 2.7.x is required (%d.%d detected)."
+                      % sys.version_info[:2])
+
+
+del sys, absolute_import
+
+__version__ = '0.13.1-a1'
